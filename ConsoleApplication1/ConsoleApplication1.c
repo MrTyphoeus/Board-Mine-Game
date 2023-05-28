5af2941ef80fdfdfc37023d28a11a9defd104512
Furkan Kesicioğlu <72072299+MrTyphoeus@users.noreply.github.com>
2023-05-28 23:59:19 +0300

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -41,23 +41,22 @@ void makeMap(char map[5][15]) {
 
 void main() {		
 
-	int oldValue, newValue = 2, character = 3;
-	int rowMove = 2, columnMove = 0, score = 0, moveNumber = 0;
+	
 
 
 	printf("\n\n\t\tWelcome !!\n\n\n\t\tThe purpose of the game is reaching to the end by going through less x\n");
 	printf("\n\n\t\tW A S D for movement\n");
-	printf("\n\n\t\tPress any key\t\t(2 Player)\n\n");
+	printf("\n\n\t\tPress any key\n\n");
 	time_t t;
 	srand(time(NULL));
 
 	system("pause");
 
 
-	int selected = 0;
+	int selected = 1;
 
 	makeMap(map);
-
+	int oldValue, newValue = 2, character = 3;
 	while (selected != 51) {
 
 		oldValue = map[0][2];											//Sets the oldValue's value where character will begin
@@ -65,6 +64,7 @@ void main() {
 
 		char wasdCoordinateChange;
 
+		int rowMove = 2, columnMove = 0, score = 100, moveNumber = 0;
 
 		while (columnMove <= 14 && columnMove >= 0) {				//Continues as long as the character does not finish the map
 
@@ -79,7 +79,7 @@ void main() {
 
 			wasdCoordinateChange = _getch(); 
 			 
-			if (wasdCoordinateChange == 100 || wasdCoordinateChange == 68) {				     //d
+			if (wasdCoordinateChange == 100 || wasdCoordinateChange == 68) {				     //d & D
 				map[rowMove][columnMove] = oldValue;            //Puts the old value to the characters place
 				columnMove += 1;							    //Increase the character's column by 1
 				oldValue = map[rowMove][columnMove];			//Holds the value of where character will go
@@ -87,7 +87,7 @@ void main() {
 			}
 
 
-			else if (wasdCoordinateChange == 97 || wasdCoordinateChange == 65) {				//a
+			else if (wasdCoordinateChange == 97 || wasdCoordinateChange == 65) {				//a & A
 				map[rowMove][columnMove] = oldValue;										
 				columnMove -= 1;					            //Decrease the character's column by 1
 				if (columnMove < 0)
@@ -98,14 +98,14 @@ void main() {
 
 
 
-			else if (wasdCoordinateChange == 115 || wasdCoordinateChange == 83) {				//s
+			else if (wasdCoordinateChange == 115 || wasdCoordinateChange == 83) {				//s & S
 				map[rowMove][columnMove] = oldValue;
 				rowMove += 1;					            	//Decrease the line with the character by 1
 				oldValue = map[rowMove][columnMove];
 				map[rowMove][columnMove] = character;
 			}
 
-			else if (wasdCoordinateChange == 119 || wasdCoordinateChange == 87) {				//w
+			else if (wasdCoordinateChange == 119 || wasdCoordinateChange == 87) {				//w &  W
 				map[rowMove][columnMove] = oldValue;
 				rowMove -= 1;					            	//Increase the line with the character by 1
 				oldValue = map[rowMove][columnMove];
@@ -115,6 +115,8 @@ void main() {
 
 			else {
 				printf("Please enter a valid value\n");
+				Sleep(200);
+				continue;
 			}
 
 
@@ -129,7 +131,13 @@ void main() {
 				oldValue = map[rowMove][columnMove];
 				map[rowMove][columnMove] = character;
 			}
+
+			if (oldValue == 'x')
+				score -= 5;
+
+
 			moveNumber += 1;
+			score -= 1;
 
 		}
 
@@ -139,19 +147,22 @@ void main() {
 		printf("\t\t\tScore: %d \t\t\t\n\n\n", score);
 		printf("\t\t\tFor different map (1)\n\t\t\tWith same map (2)\n\t\t\tTo quit (3)\n");
 
-		selected = _getch();
-		while (selected == 48) {
-			if (selected == 49)									 //Different map
-				srand(time(NULL));				                 //Sets random
+
+		while (selected == 1) {							     	 // 49 for 1, 50 for 2, 51 for 3
+			selected = _getch();
+			if (selected == 49) {							     //Different map
+				srand(time(NULL));							     //Sets random
+				break;
+			}			                                        
 			else if (selected == 50) {							 //Same map
 				srand(1);
-				continue;
+				break;
 			}
 			else if (selected == 51) {							//Quit
 				break;
 			}
 			else
-				selected = 48;
+				selected = 1;
 		}
 	}
 }
